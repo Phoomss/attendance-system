9aa33e64ba86efd89077a382b964a385c61e574f
phoomss <narongsak5az@gmail.com>
2025-01-04 18:40:16 +0700

    change design popup and nav

--- frontend/employee/navbar.php
+++ frontend/employee/navbar.php
@@ -1,34 +1,39 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">ระบบเข้าออกงาน </a>
+    <a class="navbar-brand" href="#">ระบบเข้าออกงาน</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- ข้อมูลส่วนตัว -->
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="profile.php">ข้อมูลส่วนตัว</a>
         </li>
+        <!-- การบันทึกเวลา -->
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="time_tracking.php">บันทึกเวลาเข้า-ออก</a>
         </li>
+        <!-- รายงาน -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            รายงาน
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="report_daily.php">รายงานประจำวัน</a></li>
+            <li><a class="dropdown-item" href="report_monthly.php">รายงานประจำเดือน</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="report_summary.php">รายงานสรุป</a></li>
           </ul>
         </li>
+        <!-- การตั้งค่า -->
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="settings.php">การตั้งค่า</a>
         </li>
       </ul>
+      <!-- ปุ่ม Logout -->
       <form class="d-flex">
-        <a class="btn btn-danger ms-2" type="submit" href="../../server/logout.php">Logout</a>
+        <a class="btn btn-danger ms-2" href="../../server/logout.php">ออกจากระบบ</a>
       </form>
     </div>
   </div>
--- frontend/employee/popup.php
+++ frontend/employee/popup.php
@@ -1,34 +1,26 @@
 <div class="modal fade" id="welcomeModal" tabindex="-1" aria-labelledby="welcomeModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="welcomeModalLabel">Welcome to the System</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>You've successfully logged in! Please complete your profile information.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <a href="profile.php" class="btn btn-primary">Go to Profile</a>
-                </div>
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <!-- ส่วนหัวของ Modal -->
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title" id="welcomeModalLabel">ยินดีต้อนรับสู่ระบบของเรา</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <!-- เนื้อหา Modal -->
+            <div class="modal-body">
+                <p class="text-muted">ขอบคุณที่เข้าสู่ระบบ กรุณากรอกข้อมูลส่วนตัวของคุณให้ครบถ้วน เพื่อให้การใช้งานระบบสมบูรณ์ที่สุด ขอขอบคุณสำหรับความร่วมมือ</p>
+            </div>
+            <!-- ส่วนท้ายของ Modal -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">ปิด</button>
+                <a href="profile.php" class="btn btn-primary">ไปที่ข้อมูลส่วนตัว</a>
             </div>
         </div>
     </div>
-
-    <!-- Page Content -->
-    <main class="container">
-        <div class="bg-light p-5 rounded">
-            <h1>Welcome, <?php echo htmlspecialchars($userData['name']); ?></h1>
-            <p class="lead">Your email: <?php echo htmlspecialchars($userData['email']); ?></p>
-            <p>Your role: <?php echo htmlspecialchars($userData['role']); ?></p>
-            <img src="<?php echo htmlspecialchars($userData['picture']); ?>" class="rounded" alt="profile img">
-        </div>
-    </main>
-
-    <!-- Script to trigger the modal -->
-    <script>
-        // Trigger modal when page loads
-        var modal = new bootstrap.Modal(document.getElementById('welcomeModal'));
-        modal.show();
-    </script>
+</div>
+<!-- สคริปต์สำหรับเปิด Modal -->
+<script>
+    // เปิด Modal เมื่อโหลดหน้า
+    var modal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+    modal.show();
+</script>
